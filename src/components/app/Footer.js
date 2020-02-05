d5f162b65fd880dd3013b4d49f5afa323aa6f0c0
Damian Kress <info@damiankress.de>
2020-02-05 13:45:56 +0100

    Shortened the footer for PWA installs and added a logo & updated favicon.

--- src/components/app/Footer.js
+++ src/components/app/Footer.js
@@ -7,9 +7,12 @@ const Footer = (props) => {
 		//className: 'text-center bg-primary text-white-50 rounded-top mt-2 pb-1'
 		className: 'text-center bg-white text-black-50 rounded-top py-1 ' + props.className
 	}
+	const content = (!window.matchMedia('(display-mode: standalone)').matches)
+		? <>Beim Senden Ihrer Daten erklären Sie sich mit unseren <NavLink to="/imprint">Datenschutz&shy;bestimmungen</NavLink> einverstanden. <small>&copy; 2020 projecd.org</small></>
+		: <small><NavLink to="/imprint">Impressum/Daten&shy;schutz</NavLink> &copy; 2020 projecd.org</small>
 	return (
 		<footer {...inherit}>
-			Beim Senden Ihrer Daten erklären Sie sich mit unseren <NavLink to="/imprint">Datenschutz&shy;bestimmungen</NavLink> einverstanden. <small>&copy; 2020 projecd.org</small>
+			{content}
 		</footer>
 	)
 }
