5667ef25df9448d442d446fb36f3b7e7ddc5b6e5
dkress59 <28515387+dkress59@users.noreply.github.com>
2020-08-29 16:55:57 +0200

    pipeline test

--- public/update.php
+++ public/update.php
@@ -1,3 +1,4 @@
 <?php
+echo 'Test';
 exec('cd /var/www/ifound-maps && /usr/bin/git fetch --all && /usr/bin/git checkout origin/master && /usr/bin/npm install pkg.json && /usr/bin/npm run build', $out);
 echo explode("<br />\n", $out);
