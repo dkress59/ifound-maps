cdece60af780271d4a732ee738f47713aa1abb1d
dkress59 <28515387+dkress59@users.noreply.github.com>
2020-12-27 22:29:24 +0100

    minor update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,28 @@
+const env = {
+	PORT: process.env.PORT,
+	REACT_APP_IFO_AUTH_TIME: process.env.REACT_APP_IFO_AUTH_TIME,
+	REACT_APP_IFO_API: process.env.REACT_APP_IFO_API,
+	REACT_APP_IFO_URL: process.env.REACT_APP_IFO_URL,
+	REACT_APP_IFO_MEDIA: process.env.REACT_APP_IFO_MEDIA,
+
+},
+
 module.exports = {
 	apps: [{
 		name: 'ifound-maps',
-		script: 'npx',
-		args: `serve -s build -l ${process.env.IFO_PORT}`,
-		watch: 'build',
+		// watch: 'build',
+		script: 'yarn',
+		args: 'serve -s -l $PORT',
+		interpreter: '/bin/bash',
+		env_development: {
+			...env,
+		},
+		env_production: {
+			...env,
+			HTTPS: true,
+			BROWSER: 'NONE',
+			SSL_CRT_FILE: '/etc/letsencrypt/live/ifound.one/cert.pem',
+			SSL_KEY_FILE: '/etc/letsencrypt/live/ifound.one/privkey.pem',
+		},
 	}],
 }
--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
 /usr/bin/git checkout -- .
-/usr/bin/git pull origin master
-yarn
-#yarn build
+/usr/bin/git pull
+yarn --pure-lockfile
+yarn build
+pm2 restart ecosystem.config.js --env production
